a9e51984a3ba4bb2f20d8b144a0768966b9a4219
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2014-12-13 12:34:37 +0000

    templates static, general setup

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'rango',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
